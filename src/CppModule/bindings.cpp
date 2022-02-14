e13b313da0d49ced1c0761550c1b877d2e9464a4
Matej-Chmel <58189701+Matej-Chmel@users.noreply.github.com>
2022-02-14 16:43:50 +0100

    Printing np.array.

--- src/CppModule/bindings.cpp
+++ src/CppModule/bindings.cpp
@@ -1,11 +1,43 @@
+#include <iostream>
+#include <pybind11/iostream.h>
+#include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
+#include <stdexcept>
 
 namespace py = pybind11;
 
-int add(const int a, const int b) {
-	return a + b;
+template<typename T>
+void print(const py::array_t<T, py::array::c_style | py::array::forcecast> arr) {
+	const auto buf = arr.request();
+	const T* const ptr = (const T* const)buf.ptr;
+
+	if(!buf.ndim)
+		throw std::runtime_error("No dimensions.");
+
+	const auto dimX = buf.shape[0];
+
+	if(buf.ndim == 1) {
+		for(py::ssize_t i = 0; i < dimX; i++)
+			std::cout << ptr[i] << ", ";
+
+		std::cout << '\n';
+	}
+	else if(buf.ndim == 2) {
+		const auto dimY = buf.shape[1];
+
+		for(py::ssize_t x = 0; x < dimX; x++) {
+			std::cout << '[';
+
+			for(py::ssize_t y = 0; y < dimY; y++)
+				std::cout << ptr[x * dimY + y] << ", ";
+
+			std::cout << "]\n";
+		}
+	} else
+		throw std::runtime_error("Too many dimensions.");
 }
 
 PYBIND11_MODULE(cpp_module, m) {
-	m.def("add", add);
+	py::add_ostream_redirect(m, "cppStdout");
+	m.def("print", print<float>);
 }
