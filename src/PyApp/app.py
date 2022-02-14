0c8d4ede4dd84bb6e8739be5649e90262990455c
Matej-Chmel <58189701+Matej-Chmel@users.noreply.github.com>
2022-02-14 16:43:50 +0100

    Printing np.array.

--- src/PyApp/app.py
+++ src/PyApp/app.py
@@ -1,3 +1,23 @@
 import cpp_module as m
+import numpy as np
 
-print(m.add(2, 3))
+def run(data: list[float]):
+	try:
+		with m.cppStdout(stderr=True, stdout=True):
+			m.print(np.array(data))
+	except RuntimeError as e:
+		print(e)
+
+run([1, 2, 3])
+run([
+	[1, 2, 3],
+	[4, 5, 6]
+])
+run([[
+		[1, 2, 3],
+		[4, 5, 6]
+	],[
+		[1, 2, 3],
+		[4, 5, 6]
+	]
+])
