18b4578f61917546897f4c42f6c498d72a2f1ac5
Matej-Chmel <58189701+Matej-Chmel@users.noreply.github.com>
2022-02-15 11:36:10 +0100

    Bindings for HnswlibIndexFloat.

--- src/PyApp/app.py
+++ src/PyApp/app.py
@@ -1,3 +1,30 @@
 import hnsw
+import numpy as np
 
-print(hnsw.__doc__)
+def generateData(count: int, dim: int) -> np.ndarray:
+	return np.float32(np.random.random((count, dim)))
+
+def main():
+	dim = 16
+	efConstruction = 200
+	elementCount = 10000
+	k = 10
+	M = 16
+	queryCount = max(1, elementCount // 100)
+	seed = 100
+
+	elements = generateData(elementCount, dim)
+	index = hnsw.HnswlibIndexFloat(hnsw.Space.EUCLIDEAN, dim)
+	index.init_index(elementCount, M, efConstruction, seed)
+	index.add_items(elements)
+
+	queries = generateData(queryCount, dim)
+	labels, distances = index.knn_query(queries, k)
+
+	print(type(elements))
+	print(type(queries))
+	print(labels)
+	print(distances)
+
+if __name__ == "__main__":
+	main()
