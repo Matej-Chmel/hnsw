cbda4b0ddf3eff379cd32adab21b40d32c453c6f
Matej-Chmel <58189701+Matej-Chmel@users.noreply.github.com>
2022-02-19 16:44:35 +0100

    Numpy dtype is specified by class not string. Fix: DatasetCheck reads correct HDF file. Typo: Renamed bound type of getRecall to UInt64.

--- src/DatasetCheck/DatasetCheck.py
+++ src/DatasetCheck/DatasetCheck.py
@@ -2,25 +2,21 @@ import h5py as hdf
 import numpy as np
 from pathlib import Path
 
-def check(file: hdf.File, attr: str, datasetsDir: Path, binName: str, type: str = "f4"):
-	binData = np.fromfile((datasetsDir / binName).with_suffix(".bin"), f"<{type}")
+def check(file: hdf.File, attr: str, datasetsDir: Path, binName: str, dtype: np.dtype):
+	binData = np.fromfile((datasetsDir / binName).with_suffix(".bin"), dtype=dtype)
 	hdfData = file[attr][:].flatten()
-
-	for i in range(10):
-		print(binData[i], hdfData[i])
-
 	return np.array_equal(binData, hdfData)
+
+def printCheck(file: hdf.File, attr: str, datasetsDir: Path, binName: str, dtype: np.dtype = np.float32):
+	print(f'Binary and HDF formats of dataset "{attr}" are equal: {check(file, attr, datasetsDir, binName, dtype)}.')
 
 def main():
 	datasetsDir = Path(__file__).parents[2] / "datasets"
 
-	with hdf.File(datasetsDir / "d128_tr20000_k10_te200_s100_euclidean.hdf5", "r") as f:
-		res = check(f, "neighbors", datasetsDir, "neighbors", "i8")
-		print(res)
-		res = check(f, "train", datasetsDir, "train")
-		print(res)
-		res = check(f, "test", datasetsDir, "test")
-		print(res)
+	with hdf.File(datasetsDir / "d128_tr20000_k100_te200_s100_euclidean.hdf5", "r") as f:
+		printCheck(f, "neighbors", datasetsDir, "neighbors", np.uint64)
+		printCheck(f, "train", datasetsDir, "train")
+		printCheck(f, "test", datasetsDir, "test")
 
 if __name__ == "__main__":
 	main()
